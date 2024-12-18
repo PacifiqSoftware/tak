08a15292a362272175928ee04bdf170a4cc80ddc
PacifiqSoftware <126279326+PacifiqSoftware@users.noreply.github.com>
2024-12-18 19:30:07 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,26 +1,5 @@
 @echo off
 cd C:\Users\%username%\Desktop
-mkdir Nigger
-mkdir Nigger1
-mkdir Nigger2
-mkdir Nigger3
-mkdir Nigger4
-mkdir Nigger5
-mkdir Nigger6
-mkdir Nigger7
-mkdir Nigger8
-mkdir Nigger9
-mkdir Nigger10
-mkdir Nigger11
-mkdir Nigger12
-mkdir Nigger13
-mkdir Nigger14
-mkdir Nigger15
-mkdir Nigger16
-mkdir Nigger17
-mkdir Nigger18
-mkdir Nigger19
-mkdir Nigger20
 main.vbs
 ping localhost -n 1 >NUL
 attrib +h sound.vbs
